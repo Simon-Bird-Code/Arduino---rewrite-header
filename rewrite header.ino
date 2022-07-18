b53b4ea78691b18809955e9ac0233a0af9a1f558
Simon-Bird-Code <109514172+Simon-Bird-Code@users.noreply.github.com>
2022-07-18 22:56:53 +0100

    Update rewrite header.ino

--- rewrite header.ino
+++ rewrite header.ino
@@ -1,5 +1,5 @@
 /*
-Project - rewite existing code to suitable format
+Project - rewrite existing code to suitable format
 version : 0v01
 Author Simon J Bird
  */
@@ -30,31 +30,41 @@ Author Simon J Bird
 // what you should have here is a wiring diagram 
 // ALL the pins used, even if commented out, as hardware only
 
-                            //  physical Pin 7  |   POWERVCC  |
-                            //  physical Pin 8  |   POWERGRD1 |
-                            //  physical Pin 9  |   Crystal   |   
-                            //  physical Pin 10 |   Crystal   | 
+#define UNUSED1        1    //  physical Pin 7  |   RESET| PC6    |
+#define UNUSED2        2    //  physical Pin 8  |   RXD  | PD0    |
+#define UNUSED3        3    //  physical Pin 9  |   TXD  | PD1    |   
+#define UNUSED4        4    //  physical Pin 10 |   INT0 | PD2    | 
 
 
-#define B_FORWARDS 13       //  physical Pin 7  |   GPIO      |
-#define B_BACKWARDS 12      //  physical Pin 7  |   POWERVCC  | 
-#define B_BOOST1 11         //  physical Pin 7  |   POWERVCC  |
+#define B_FORWARDS     5    //  physical Pin 7  |   PWM | PD3     |
+#define B_BACKWARDS    6    //  physical Pin 7  |       | PD4     |        
+#define POWERVCC       91   //  physical Pin 7  |   POWERVCC      |
 
-#define B_LEFT 21           //  physical Pin 7  |   POWERVCC  |
-#define B_RIGHT 22          //  physical Pin 7  |   POWERVCC  |
-#define B_BOOST2 23         //  physical Pin 7  |   POWERVCC  |
+#define GROUND         92   //  physical Pin 7  |   GROUND        |
+#define UNUSED5        93   //  physical Pin 7  |   CRYSTAL | PB6 |
+#define UNUSED6        94   //  physical Pin 7  |   CRYSTAL | PB7 |
 
-#define B_UP 18             //  physical Pin 7  |   POWERVCC  |
-#define B_DOWN 19           //  physical Pin 7  |   POWERVCC  |
-#define B_BOOST3 20         //  physical Pin 7  |   POWERVCC  |
-            
-#define B_TLEFT 10          //  physical Pin 7  |   POWERVCC  |
-#define B_TRIGHT 9          //  physical Pin 7  |   POWERVCC  |
-#define B_BOOST4 6          //  physical Pin 7  |   POWERVCC  |
+#define B_UP 18        7    //  physical Pin 7  |   PD5           |
+#define B_DOWN         8    //  physical Pin 7  |   PD6           |
+#define B_TRIGHT 20    9    //  physical Pin 7  |   PD7           |
+#define B_TLEFT 10     10   //  physical Pin 7  |   PB0           |
 
-#define B_EXTRA1 5          //  physical Pin 7  |   POWERVCC  |
-#define B_EXTRA2 3          //  physical Pin 7  |   POWERVCC  |
-#define B_BOOST5 2          //  physical Pin 7  |   POWERVCC  |
+#define B_BOOST1 9     11   //  physical Pin 7  |   PC5  | ANLG 5 |
+#define B_BOOST2 6     12   //  physical Pin 7  |   PC4  | ANLG 4 |
+
+#define B_EXTRA1 5     13   //  physical Pin 7  |   PC3  | ANLG 3 |
+#define B_EXTRA2 3     14   //  physical Pin 7  |   PC2  | ANLG 2 |
+#define B_BOOST3 2     15   //  physical Pin 7  |   PC1  | ANLG 1 |
+#define B_BOOST4 2     16   //  physical Pin 7  |   PC0  | ANLG 0 |
+
+#define GROUND2        95   //  physical Pin 7  |   GROUND        |
+#define ANLG_REF       96   //  physical Pin 7  |   ANLG REF      |
+#define UNUSED5        97   //  physical Pin 7  |   POWERVCC      |
+#define UNUSED6        17   //  physical Pin 7  |   PD13 | PWM    |
+#define UNUSED7 21     18   //  physical Pin 7  |   PD12 | PWM    |
+#define UNUSED8 21     19   //  physical Pin 7  |   PD11 | PWM    |
+#define UNUSED9 21     20   //  physical Pin 7  |   PD10 | PWM    |
+#define UNUSED10 21    21   //  physical Pin 7  |   PD9  | PWM    |
 
 #define B_INTERACT 1 
 
